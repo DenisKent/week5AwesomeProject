0e39221f8d1f95e06489bf134ec2a302e27ca7ef
Matt Hall <matthall8@gmail.com>
2018-07-31 16:11:47 +0300

    Updated route file with comments

--- src/router.js
+++ src/router.js
@@ -1,5 +1,9 @@
 const handlers = require('./handlers');
 
+/* Router fn to deal with 4 requests - the homepage, an asset (e.g. CSS file),
+a client data request and an else which produces a 404 page. These requests call
+on functions in the handlers file to produce a response. */
+
 const router = (request, response) => {
   const url = request.url;
   if (url === '/') {

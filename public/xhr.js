60cc8f1a10cba559d889d05158b255075a59e049
Matt Hall <matthall8@gmail.com>
2018-07-31 17:41:05 +0300

    Added comments to the XHR file

--- public/xhr.js
+++ public/xhr.js
@@ -1,15 +1,23 @@
 /* eslint-disable*/
 
+/* Function to get a request from the server based on the user's input */
+
 function serverDataRequest(userInput) {
+  /* Create new request */
   var xhr = new XMLHTTPRequest();
-  var url = '/getData';
+  /* URL is get-data */
+  var url = '/get-data';
+  /* Setting create request header for form input */
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  /* Setting call back function for when response is received  */
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       // Typical action to be performed when the document is ready:
       document.getElementById('test').innerHTML = xhr.responseText;
     }
   };
+  /* Open POST request with URL  */
   xhr.open('POST', url, true);
+  /* Send the user's input in the body of the request */
   xhr.send(userInput);
 }

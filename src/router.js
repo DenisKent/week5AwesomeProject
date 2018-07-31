6e59134787fe91fb73090c1dd0d253c583359bf7
Matt Hall <matthall8@gmail.com>
2018-07-31 16:51:40 +0300

    Updated assets handler to take .js files

--- src/router.js
+++ src/router.js
@@ -1,16 +1,16 @@
 const handlers = require('./handlers');
 
+const assetURLs = ['/dom.js', '/style.css', '/xhr.js'];
 /* Router fn to deal with 4 requests - the homepage, an asset (e.g. CSS file),
 a client data request and an else which produces a 404 page. These requests call
 on functions in the handlers file to produce a response. */
-
 const router = (request, response) => {
   const url = request.url;
   if (url === '/') {
     handlers.homePage(response);
   } else if (url.indexOf('/getData') === 0) {
     handlers.searchHandler(url, response);
-  } else if (url.includes('style.css')) {
+  } else if (assetURLs.includes(url)) {
     handlers.assetsHandler(url, response);
   } else {
     handlers.notFound(response);

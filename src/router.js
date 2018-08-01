d1216eec2158970b0fd43d934d1622d44302d595
DenisKent <kentdenis6@hotmail.co.uk>
2018-08-01 03:12:32 +0300

    CSS changes, including moving background

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 const handlers = require('./handlers');
 
-const assetURLs = ['/index.html', '/dom.js', '/style.css', '/xhr.js'];
+const assetURLs = ['/index.html', '/dom.js', '/style.css', '/xhr.js', '/background.js'];
 /* Router fn to deal with 4 requests - the homepage, an asset (e.g. CSS file),
 a client data request and an else which produces a 404 page. These requests call
 on functions in the handlers file to produce a response. */

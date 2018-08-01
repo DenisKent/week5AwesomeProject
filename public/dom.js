2cee66749fd061fd2499f01afd78fe5ec78930d2
Matt Hall <matthall8@gmail.com>
2018-08-01 19:52:18 +0300

    Updated so geolocation is now passing to the server

--- public/dom.js
+++ public/dom.js
@@ -1,5 +1,29 @@
 /* eslint-disable*/
 
+window.addEventListener('load', function(event) {
+  // var x = document.getElementById('demo');
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(showPosition);
+  // } else {
+  //   x.innerHTML = 'Geolocation is not supported by this browser.';
+  // }
+  // function showPosition(position) {
+  //   x.innerHTML =
+  //     'Latitude: ' + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude;
+  // }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(localPollutionDataRequest);
+  } else {
+    x.innerHTML = 'Geolocation is not supported by this browser.';
+  }
+});
+
+function localPollutionDataRequest(position) {
+  var coordinates = { lat: position.coords.latitude, long: position.coords.longitude };
+  console.log(coordinates);
+  pollutionDataRequest(coordinates, console.log);
+}
+
 /* Assigned the submitBtn from the DOM to the var */
 var cityInput = document.getElementById('cityInputBox');
 var citiesDropdown = document.getElementById('citiesDropdown');
@@ -7,44 +31,43 @@ var citiesDropdown = document.getElementById('citiesDropdown');
 
 cityInput.addEventListener('keyup', function() {
   /* Collects the user's input from the box */
-  var usrInput = cityInput.value
+  var usrInput = cityInput.value;
   /* Calls the fn severDataRequest (in xhr.js) with the user's input */
-  if (usrInput){
-    autocomplete(usrInput,populateDropdown);
+  if (usrInput) {
+    autocomplete(usrInput, populateDropdown);
   }
 });
 
-var populateDropdown = function(cityList){
+var populateDropdown = function(cityList) {
   removeChildren(citiesDropdown);
-  cityList.forEach(function(city){
+  cityList.forEach(function(city) {
     /* Create an option element for each city. Each option elemnt has two spans in it with the city name and city country*/
-    var option = document.createElement("option");
-    option.value = city.name
-    var country = document.createElement("span")
-    country.textContent = city["Alternate country code"];
+    var option = document.createElement('option');
+    option.value = city.name;
+    var country = document.createElement('span');
+    country.textContent = city['Alternate country code'];
     option.appendChild(country);
     citiesDropdown.appendChild(option);
   });
-}
+};
 
 function removeChildren(obj) {
   while (obj.hasChildNodes()) {
     obj.removeChild(obj.firstChild);
   }
-};
+}
 
 function cityDataRequest() {
-    var val = document.getElementById("cityInputBox").value;
-    var opts = document.getElementById("citiesDropdown").childNodes;
-    for (var i = 0; i < opts.length; i++) {
-      if (opts[i].value === val) {
-        // An item was selected from the list!
-        // yourCallbackHere()
-        alert(opts[i].value);
-        break;
-      }
+  var val = document.getElementById('cityInputBox').value;
+  var opts = document.getElementById('citiesDropdown').childNodes;
+  for (var i = 0; i < opts.length; i++) {
+    if (opts[i].value === val) {
+      // An item was selected from the list!
+      // yourCallbackHere()
+      alert(opts[i].value);
+      break;
     }
   }
+}
 
-
-  // pollutionDataRequest(userInput);
+// pollutionDataRequest(userInput);
--- public/xhr.js
+++ public/xhr.js
@@ -1,7 +1,7 @@
 /* eslint-disable*/
 /* Function to get a request from the server based on the user's input */
 
-function pollutionDataRequest (userInput) {
+function pollutionDataRequest(coordinates) {
   /* Create new request */
   var xhr = new XMLHttpRequest();
   /* URL is get-pollution-data*/
@@ -11,22 +11,23 @@ function pollutionDataRequest (userInput) {
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       // Action to be performed when the document is ready:
-      console.log(JSON.parse(xhr.responseText));
+      console.log(xhr.responseText);
     }
   };
   /* Open POST request with URL  */
   xhr.open('POST', url, true);
   /* Setting create request header for form input */
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.setRequestHeader('Content-type', 'application/json');
   /* Send the user's input in the body of the request */
-  xhr.send(userInput);
+  console.log(JSON.stringify(coordinates));
+  xhr.send(JSON.stringify(coordinates));
 }
 
-function autocomplete (userInput,cb){
+function autocomplete(userInput, cb) {
   /* Create new request */
   var xhr = new XMLHttpRequest();
   /* URL is get-pollution-data, we pass through the query to the end of it. It most also be encoded so it can be decoded on the back-end */
-  var url = encodeURI('/autocomplete-city?q='+userInput);
+  var url = encodeURI('/autocomplete-city?q=' + userInput);
 
   /* Setting call back function for when response is received  */
   xhr.onreadystatechange = function() {
